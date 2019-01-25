1f773f9feb4f23883f054e484c92542d6d44497e
Babu Lal Meena <babulal@bizongo.in>
2019-01-25 19:03:00 +0530

    add first line

---
+++ f/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('f')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
